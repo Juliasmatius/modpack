7d0fabbf5829ccdf924113583c68d2d6b5365907
Juliasmatius <80146546+Juliasmatius@users.noreply.github.com>
2022-12-21 11:33:24 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 @echo off
 cd ‰appdata%/.minecraft/mods/portablegit/bin/
-start "Updating" wingit.exe clone --guiet https://github.com/Juliasmatius/modpack.git
+start "Updating" git.exe clone --guiet https://github.com/Juliasmatius/modpack.git
